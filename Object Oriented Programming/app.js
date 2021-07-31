8afaf11657224deb1f4a5b9f19208bffa9d36827
E-Ozdemir <emreclarusway@gmail.com>
2021-07-31 16:55:59 +0200

    OOP

---
+++ Object Oriented Programming/app.js
@@ -0,0 +1,25 @@
+// const person ={
+//     firstName: 'Emre',
+//     secondName: 'Özdemir',
+//     skills: ['swimming','hiking'],
+//     employStatus: true,
+//     birthYear: 1989,
+//     currentAge = function(bYear){
+//         return 2021 - bYear;
+//     } 
+// }
+
+//------------
+
+const myObject = {
+    x : 5,
+    y : 6,
+    'okul adi': 'saint joseph',
+    while : 42,
+}
+myObject.dob = 2014;
+myObject['baba adi'] = 'rifat';
+
+console.log(`While==>`, myObject.while);
+console.log(`okul adi==>`, myObject['okul adi']);
+console.log(`baba adi==>`, myObject['baba adi'] ) ;
